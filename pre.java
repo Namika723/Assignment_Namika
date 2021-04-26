e6308a18dacaec89e253705a37e4422fbfac3da3
Namika723 <83168014+Namika723@users.noreply.github.com>
2021-04-27 00:12:38 +0530

    Add files via upload

---
+++ pre.java
@@ -0,0 +1,11 @@
+package vardemo;
+
+public class pre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i=20+ -3*5/8;
+        System.out.print(i);
+	}
+
+}
