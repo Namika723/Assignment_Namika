7e6b0f724a6e6b7baa22139ef1f5e58892894501
Namika723 <83168014+Namika723@users.noreply.github.com>
2021-04-27 00:13:06 +0530

    Add files via upload

---
+++ opclass.java
@@ -0,0 +1,11 @@
+package vardemo;
+
+public class opclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i=5+15/3*2-8%3;
+    System.out.print(i);
+	}
+
+}
