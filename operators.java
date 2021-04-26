77a7fed754a88d97930ff24084af5e8870446a85
Namika723 <83168014+Namika723@users.noreply.github.com>
2021-04-27 00:13:27 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,13 @@
+package vardemo;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=(55+9)%9;
+		System.out.print(i);
+		
+
+	}
+
+}
